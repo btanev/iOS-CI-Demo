78ca10b3c9cdd4894dba2c38cac3f908d36a2caa
Boby Tanev <btanev@gmail.com>
2018-03-18 17:07:55 +0200

    added versioning to the sample app

--- CI Demo/ViewController.swift
+++ CI Demo/ViewController.swift
@@ -10,9 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var versionLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // app version
+        let appVersion: String = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as! String
+        let buildNumber: String = Bundle.main.object(forInfoDictionaryKey: "CFBundleVersion") as! String
+        
+        let appVersionFull: String = "v\(appVersion).\(buildNumber)"
+        self.versionLabel.text = appVersionFull
     }
 
     override func didReceiveMemoryWarning() {
